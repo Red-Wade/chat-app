84502945d0817c661fd336cf8d6f3c91fb32aa8d
Ismail Abriak <isma.ao.inf@gmail.com>
2023-03-11 21:38:11 +0100

    Working

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -15,6 +15,6 @@ class Chat extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class, 'users_chats');
+        return $this->belongsToMany(User::class, 'users_chats')->withTimestamps();
     }
 }
