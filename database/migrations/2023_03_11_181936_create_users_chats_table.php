5e36ec1269b0a3aba0b91243fd8cab1bb87b7b67
Ismail Abriak <isma.ao.inf@gmail.com>
2023-03-11 19:19:56 +0100

    modified:   database/migrations/2023_03_11_175712_create_chats_table.php 	new file:   database/migrations/2023_03_11_181936_create_users_chats_table.php

--- database/migrations/2023_03_11_181936_create_users_chats_table.php
+++ database/migrations/2023_03_11_181936_create_users_chats_table.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('chats', function (Blueprint $table) {
+        Schema::create('users_chats', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('name')->default('Chat'); 
         });
     }
 
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('chats');
+        Schema::dropIfExists('users_chats');
     }
 };
