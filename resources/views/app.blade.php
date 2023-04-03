4188b81d2c9a27fe6f50abbb090f5c100c8e0e43
wade079 <99978276+wade079@users.noreply.github.com>
2023-04-03 19:34:53 +0200

    feat: add favicon and meta description

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta http-equiv="Content-Security-Policy" content="block-all-mixed-content">
+    <meta name="description" content="Únete a nuestra aplicación de chat en línea para conectarte con amigos y familiares de todo el mundo. Envía mensajes, comparte archivos y realiza llamadas de voz y video de alta calidad con facilidad. Nuestra aplicación es fácil de usar y segura, con chats que se borran automáticamente después de 30 días. Si necesitas ayuda o tienes preguntas, estamos aquí para ayudarte. ¡Únete a nuestra comunidad hoy mismo y disfruta de todas las características que ofrecemos!">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
